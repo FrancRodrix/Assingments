c3e2e81e720985e1eba7af07794b94d809c1074b
Machd <machd@Machds-MacBook-Pro.local>
2021-10-20 13:49:28 +0530

    asteriod project

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import Navigation from './src/Navigation/Navigation'
-
-export default function App() {
-  return (
-   <Navigation/>
-  )
-}
